767c7b8f00cf5d4dd96b8fa4321194c4f1370a80
Vladimir Alekseev <som3t1me5@gmail.com>
2018-07-16 14:19:02 +0300

    chapter 3.1 LeftistHeap refactor to weight-biased leftist

--- src/main/scala/me/valekseev/heap/LeftistHeap.scala
+++ src/main/scala/me/valekseev/heap/LeftistHeap.scala
@@ -10,31 +10,34 @@ import scala.collection.immutable.{Nil => SNil}
 /**
   * @author sss3 (Vladimir Alekseev)
   */
-
 trait LeftistHeap[T]
 
 object LeftistHeap {
 
   private final case class Nil[T]() extends LeftistHeap[T]
   private final case class Node[T](rank: Int, x: T, left: LeftistHeap[T], right: LeftistHeap[T])
-      extends LeftistHeap[T]
+      extends LeftistHeap[T] {
+    override def toString: String = s"Node($x, ${left.toString}, ${right.toString})"
+  }
 
   implicit def heap[T: Order]: Heap[T, LeftistHeap] = new LHeap[T]
 
   def fromList[T: Order](list: List[T]): LeftistHeap[T] = fromList(list.map(Node(1, _, Nil(), Nil())))
 
   @tailrec
-  private def fromList[T](list: List[LeftistHeap[T]])(implicit heap: Heap[T, LeftistHeap]): LeftistHeap[T] = reduceList(list) match {
-    case SNil => heap.empty
-    case x:: SNil => x
-    case x::y::SNil => heap.merge(x, y)
-    case xs => fromList(xs)
-  }
+  private def fromList[T](list: List[LeftistHeap[T]])(implicit heap: Heap[T, LeftistHeap]): LeftistHeap[T] =
+    reduceList(list) match {
+      case SNil           => heap.empty
+      case x :: SNil      => x
+      case x :: y :: SNil => heap.merge(x, y)
+      case xs             => fromList(xs)
+    }
 
-  private def reduceList[T](list: List[LeftistHeap[T]])(implicit heap: Heap[T, LeftistHeap]): List[LeftistHeap[T]] = list match {
-    case SNil => list
-    case x::SNil => x :: SNil
-    case x::y::xs => heap.merge(x, y) :: reduceList(xs)
+  private def reduceList[T](list: List[LeftistHeap[T]])(
+      implicit heap: Heap[T, LeftistHeap]): List[LeftistHeap[T]] = list match {
+    case SNil         => list
+    case x :: SNil    => x :: SNil
+    case x :: y :: xs => heap.merge(x, y) :: reduceList(xs)
   }
 
   private class LHeap[T: Order] extends Heap[T, LeftistHeap] {
@@ -47,24 +50,35 @@ object LeftistHeap {
 
     override def insert(x: T, heap: LeftistHeap[T]): LeftistHeap[T] = heap match {
       case Nil() => Node(1, x, Nil(), Nil())
-      case n @ Node(ra, v, l, r) => x.comparison(v) match {
-        case Comparison.LessThan => Node(1, x, n, Nil())
-        case _ => rank(l).compareTo(rank(r)) match {
-          case compare if compare > 0 =>
-            val nR = insert(x, r)
-            Node(rank(nR) + 1, v, l, nR)
-          case _ => Node(ra, v, insert(x, l), r)
+      case n @ Node(ra, v, l, r) =>
+        x.comparison(v) match {
+          case Comparison.GreaterThan =>
+            rank(l).compareTo(rank(r)) match {
+              case compare if compare > 0 =>
+                Node(ra + 1, v, l, insert(x, r))
+              case _ => Node(ra + 1, v, insert(x, l), r)
+            }
+          case _ => Node(ra + 1, x, n, Nil())
         }
-      }
     }
 
     override def merge(left: LeftistHeap[T], right: LeftistHeap[T]): LeftistHeap[T] = (left, right) match {
       case (h, Nil()) => h
       case (Nil(), h) => h
-      case (h1 @ Node(_, x, l1, r1), h2 @ Node(_, y, l2, r2)) =>
+      case (h1 @ Node(ra1, x, l1, r1), h2 @ Node(ra2, y, l2, r2)) =>
         x.comparison(y) match {
-          case Comparison.GreaterThan => makeT(y, l2, merge(h1, r2))
-          case _                      => makeT(x, l1, merge(r1, h2))
+          case Comparison.GreaterThan =>
+            if (rank(l2) >= rank(r2) + ra1) {
+              Node(ra1 + ra2, y, l2, merge(h1, r2))
+            } else {
+              Node(ra1 + ra2, y, merge(l2, h1), r2)
+            }
+          case _                      =>
+            if (rank(l1) >= rank(r1) + ra2) {
+              Node(ra1 + ra2, x, l1, merge(r1, h2))
+            } else {
+              Node(ra1 + ra2, x, merge(l1, h2), r1)
+            }
         }
     }
 
@@ -79,12 +93,6 @@ object LeftistHeap {
     }
 
     override def ord: Order[T] = Order[T]
-
-    private def makeT(x: T, left: LeftistHeap[T], right: LeftistHeap[T]): LeftistHeap[T] =
-      rank(left).compareTo(rank(right)) match {
-        case compare if compare >= 0 => Node(rank(right) + 1, x, left, right)
-        case _                       => Node(rank(left) + 1, x, right, left)
-      }
 
     private def rank(h: LeftistHeap[T]): Int = h match {
       case Nil()            => 0
