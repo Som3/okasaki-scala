70096e0ac85a8f4a1bdf71090c8b3c9fa642c788
Vladimir Alekseev <som3t1me5@gmail.com>
2018-07-28 23:38:34 +0300

    refactoring

--- src/main/scala/me/valekseev/heap/Heap.scala
+++ src/main/scala/me/valekseev/heap/Heap.scala
@@ -2,6 +2,8 @@ package me.valekseev.heap
 
 import cats.Order
 
+import scala.annotation.tailrec
+
 /**
   * @author sss3 (Vladimir Alekseev)
   */
@@ -20,5 +22,27 @@ trait Heap[T, R[_]] {
 object Heap {
 
   def apply[T, R[_]](implicit heap: Heap[T, R]): Heap[T, R] = heap
+
+  def fromList[R[_]]: FromListVia[R] = new FromListVia[R]
+
+  class FromListVia[R[_]] {
+    def apply[T: Order](list: List[T])(implicit heap: Heap[T, R]): R[T] = fromList(list.map(heap.insert(_, heap.empty)))
+
+    @tailrec
+    private def fromList[T](list: List[R[T]])(implicit heap: Heap[T, R]): R[T] =
+      reduceList(list) match {
+        case Nil           => heap.empty
+        case x :: Nil      => x
+        case x :: y :: Nil => heap.merge(x, y)
+        case xs            => fromList(xs)
+      }
+
+    private def reduceList[T](list: List[R[T]])(
+      implicit heap: Heap[T, R]): List[R[T]] = list match {
+      case Nil         => list
+      case x :: Nil    => x :: Nil
+      case x :: y :: xs => heap.merge(x, y) :: reduceList(xs)
+    }
+  }
 
 }
--- src/main/scala/me/valekseev/heap/LeftistHeap.scala
+++ src/main/scala/me/valekseev/heap/LeftistHeap.scala
@@ -4,9 +4,6 @@ import cats.Order
 import cats.kernel.Comparison
 import cats.syntax.order._
 
-import scala.annotation.tailrec
-import scala.collection.immutable.{Nil => SNil}
-
 /**
   * @author sss3 (Vladimir Alekseev)
   */
@@ -21,24 +18,6 @@ object LeftistHeap {
   }
 
   implicit def heap[T: Order]: Heap[T, LeftistHeap] = new LHeap[T]
-
-  def fromList[T: Order](list: List[T]): LeftistHeap[T] = fromList(list.map(Node(1, _, Nil(), Nil())))
-
-  @tailrec
-  private def fromList[T](list: List[LeftistHeap[T]])(implicit heap: Heap[T, LeftistHeap]): LeftistHeap[T] =
-    reduceList(list) match {
-      case SNil           => heap.empty
-      case x :: SNil      => x
-      case x :: y :: SNil => heap.merge(x, y)
-      case xs             => fromList(xs)
-    }
-
-  private def reduceList[T](list: List[LeftistHeap[T]])(
-      implicit heap: Heap[T, LeftistHeap]): List[LeftistHeap[T]] = list match {
-    case SNil         => list
-    case x :: SNil    => x :: SNil
-    case x :: y :: xs => heap.merge(x, y) :: reduceList(xs)
-  }
 
   private class LHeap[T: Order] extends Heap[T, LeftistHeap] {
     override def empty: LeftistHeap[T] = Nil()
