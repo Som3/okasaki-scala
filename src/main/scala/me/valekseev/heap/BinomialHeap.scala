5a599f671a474cff884f5bf7240a2a5705e04647
Vladimir Alekseev <som3t1me5@gmail.com>
2018-07-29 08:50:01 +0300

    Chapter 3.2 ex 3.6

--- src/main/scala/me/valekseev/heap/BinomialHeap.scala
+++ src/main/scala/me/valekseev/heap/BinomialHeap.scala
@@ -16,12 +16,13 @@ object BinomialHeap {
 
   implicit def biHeap[T: Order]: Heap[T, BinomialHeap] = new BHeap[T]
 
-  private final case class BHeapRep[T](tree: List[Node[T]]) extends BinomialHeap[T] {
-    override def toString: String = s"BinomialHeap( $tree )"
+  private final case class BHeapRep[T](tree: List[Tree[T]]) extends BinomialHeap[T] {
+    override def toString: String = s"BinomialHeap($tree)"
   }
-  private final case class Node[T](rank: Int, x: T, child: List[Node[T]]) {
-    override def toString: String = s"[$x, $rank, $child]"
+  private final case class Node[T](x: T, child: List[Node[T]]) {
+    override def toString: String = s"[$x, $child]"
   }
+  private final case class Tree[T](rank: Int, root: Node[T])
 
   private class BHeap[T: Order] extends Heap[T, BinomialHeap] {
     override def empty: BinomialHeap[T] = BHeapRep(List.empty)
@@ -31,65 +32,66 @@ object BinomialHeap {
       case _                        => false
     }
 
-    override def insert(x: T, heap: BinomialHeap[T]): BinomialHeap[T] = insTree(Node(0, x, List.empty), heap)
+    override def insert(x: T, heap: BinomialHeap[T]): BinomialHeap[T] =
+      insTree(Tree(0, Node(x, List.empty)), heap)
 
     override def merge(left: BinomialHeap[T], right: BinomialHeap[T]): BinomialHeap[T] = (left, right) match {
       case (l, BHeapRep(Nil)) => l
       case (BHeapRep(Nil), r) => r
       case (lh @ BHeapRep(l :: ls), rh @ BHeapRep(r :: rs)) =>
-        rank(l).comparison(rank(r)) match {
+        l.rank.comparison(r.rank) match {
           case Comparison.LessThan    => BHeapRep(l :: unwrap(merge(BHeapRep(ls), rh)))
           case Comparison.GreaterThan => BHeapRep(r :: unwrap(merge(lh, BHeapRep(rs))))
-          case _                      => insTree(link(l, r), merge(BHeapRep(ls), BHeapRep(rs)))
+          case _                      => insTree(Tree(l.rank + 1, link(l.root, r.root)), merge(BHeapRep(ls), BHeapRep(rs)))
         }
     }
 
     override def findMin(heap: BinomialHeap[T]): Option[T] = heap match {
       case BHeapRep(Nil) => Option.empty
-      case BHeapRep(xs)  => Some(xs.map(_.x).min(ord.toOrdering))
+      case BHeapRep(xs)  => Some(xs.map(_.root.x).min(ord.toOrdering))
     }
 
     override def deleteMin(heap: BinomialHeap[T]): BinomialHeap[T] = heap match {
       case BHeapRep(Nil) => heap
       case h =>
         val r = removeMinTree(h)
-        merge(BHeapRep(r._1.child), r._2)
+        val trees = r._1.root.child.zipWithIndex.map {
+          case (n, i) => Tree(r._1.rank - i - 1, n)
+        }.reverse
+        merge(BHeapRep(trees), r._2)
     }
 
     override def ord: Order[T] = Order[T]
 
     @tailrec
-    private def insTree(n: Node[T], heap: BinomialHeap[T]): BinomialHeap[T] = heap match {
-      case BHeapRep(Nil) => BHeapRep(List(n))
+    private def insTree(tree: Tree[T], heap: BinomialHeap[T]): BinomialHeap[T] = heap match {
+      case BHeapRep(Nil) => BHeapRep(List(tree))
       case BHeapRep(ts @ bh :: bhs) =>
-        rank(n).compareTo(rank(bh)) match {
-          case c if c < 0 => BHeapRep(n :: ts)
-          case _          => insTree(link(n, bh), BHeapRep(bhs))
+        tree.rank.compareTo(bh.rank) match {
+          case c if c < 0 => BHeapRep(tree :: ts)
+          case _          => insTree(Tree(tree.rank + 1, link(tree.root, bh.root)), BHeapRep(bhs))
         }
     }
 
     private def link(left: Node[T], right: Node[T]): Node[T] = (left, right) match {
-      case (t1 @ Node(_, x1, c1), t2 @ Node(_, x2, c2)) =>
+      case (t1 @ Node(x1, c1), t2 @ Node(x2, c2)) =>
         x1.comparison(x2) match {
           case Comparison.GreaterThan =>
-            Node(rank(t1) + 1, x2, t1 :: c2)
-          case _ => Node(rank(t1) + 1, x1, t2 :: c1)
+            Node(x2, t1 :: c2)
+          case _ => Node(x1, t2 :: c1)
         }
     }
 
-    private def unwrap(heap: BinomialHeap[T]): List[Node[T]] = heap match {
+    private def unwrap(heap: BinomialHeap[T]): List[Tree[T]] = heap match {
       case BHeapRep(xs) => xs
       case _            => List.empty
     }
 
-    private def removeMinTree(heap: BinomialHeap[T]): (Node[T], BinomialHeap[T]) = heap match {
+    private def removeMinTree(heap: BinomialHeap[T]): (Tree[T], BinomialHeap[T]) = heap match {
       case BHeapRep(x :: Nil) => (x, BHeapRep(Nil))
       case BHeapRep(x :: xs) =>
         val (t, ts) = removeMinTree(BHeapRep(xs))
-        if (ord.lteqv(x.x, t.x)) { (x, BHeapRep(xs)) } else { (t, BHeapRep(x :: unwrap(ts))) }
+        if (ord.lteqv(x.root.x, t.root.x)) { (x, BHeapRep(xs)) } else { (t, BHeapRep(x :: unwrap(ts))) }
     }
-
-    private def rank(n: Node[T]): Int = n.rank
-
   }
 }
